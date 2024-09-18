3cef72d7594168dc89ed424419e9940695a65589
Max Unanian <Zeptorqyiad99@gmail.com>
2024-09-18 16:45:10 +0500

    Finished with main section

---
+++ js/script.js
@@ -0,0 +1,10 @@
+new Swiper('.swiper', {
+   slidesPerView: 3,
+   spaceBetween: 60,
+   loop: true,
+
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+})
