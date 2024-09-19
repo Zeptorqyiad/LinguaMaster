b06849e58218c85b06677f4f29d8de0422867866
Max Unanian <Zeptorqyiad99@gmail.com>
2024-09-20 01:14:09 +0500

    Finished with project

--- js/script.js
+++ js/script.js
@@ -1,26 +1,21 @@
 // Swiper
 
-new Swiper('.swiper', {
+new Swiper('.swiper-services', {
    slidesPerView: 3,
+   spaceBetween: 60,
    loop: true,
    breakpoints: {
       400: {
          slidesPerView: 1,
-         spaceBetween: 20,
-      },
-      640: {
-         slidesPerView: 2,
-         spaceBetween: 40,
       },
       1240: {
          slidesPerView: 3,
-         spaceBetween: 60,
       },
    },
 
    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.custom-next',
+      prevEl: '.custom-prev',
    },
 })
 
@@ -41,4 +36,16 @@ burgerLine.addEventListener('click', () => {
 $('.input-file input[type=file]').on('change', function () {
    let file = this.files[0]
    $(this).closest('.input-file').find('.input-file-text').html(file.name)
+})
+
+// Скрытые блоги
+
+let showButton = document.querySelector('.blog__btn')
+let blog = document.querySelector('.hiddenBlog')
+
+showButton.addEventListener('click', () => {
+   if (showButton) {
+      blog.classList.add('showBlog')
+      showButton.classList.add('hiddenBlog')
+   }
 })
