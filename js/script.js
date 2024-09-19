7b97d44bb16a906d8208bd186599e108533e5f9f
Max Unanian <Zeptorqyiad99@gmail.com>
2024-09-19 13:39:40 +0500

    Finished with responsive header block

--- js/script.js
+++ js/script.js
@@ -8,3 +8,16 @@ new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
    },
 })
+
+// Бургер меню
+
+let burgerLine = document.querySelector('.burger__line')
+let burgerList = document.querySelector('.menu__icon')
+
+burgerLine.addEventListener('click', () => {
+   if (burgerLine) {
+      document.body.classList.add('__lock')
+      burgerLine.classList.toggle('active')
+      burgerList.classList.toggle('active')
+   }
+})
